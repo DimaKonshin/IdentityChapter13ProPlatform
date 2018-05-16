fac1b5c03014a2fd57794ac87babdef0b14329f6
Dima <some@mail>
2018-05-16 18:43:21 +0400

    4 Creating the User Class. Creating the Database Context Class

---
+++ IdentityChapter13ProPlatform/Infrastructure/AppIndentityDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    public class AppIndentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIndentityDbContext():base("IdentityDb")
+        {
+
+        }
+
+        static AppIndentityDbContext()
+        {
+            Database.SetInitializer<AppIndentityDbContext>(new IdentityDbInit());
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIndentityDbContext>
+    {
+        protected override void Seed(AppIndentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIndentityDbContext context)
+        {
+            
+        }
+    }
+}
